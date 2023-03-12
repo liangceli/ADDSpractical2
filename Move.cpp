0ca1c006523389d4be7994867bb1386a7a9b5fd1
Liangce Li <liangceli>
2023-03-13 03:33:31 +1030

    k

--- Move.cpp
+++ Move.cpp
@@ -1,15 +1,1 @@
-/*#include <iostream>
-#include <string>
-#include"Move.h"
-using namespace std;
-
-
-
-string Move :: getName(){
-    return "none";
-}
-/*
-int Move :: winLose(string oMove){
-    return 0;
-}*/
 
